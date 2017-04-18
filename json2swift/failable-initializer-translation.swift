5df2ad9f8d91f4d759d266432be6fb1245d3a52e
Remigiusz Herba <remigiusz.herba@allegrogroup.com>
2017-04-18 17:33:53 +0200

    Added code which generates implementation of serialisable protocol.

--- json2swift/failable-initializer-translation.swift
+++ json2swift/failable-initializer-translation.swift
@@ -6,26 +6,8 @@
 //  Copyright © 2016 iJoshSmith. All rights reserved.
 //
 
-// MARK: - JSONElementSchema --> SwiftFailableInitializer
-internal extension SwiftFailableInitializer {
-    static func create(forStructBasedOn jsonElementSchema: JSONElementSchema) -> SwiftFailableInitializer {
-        let attributeMap = jsonElementSchema.attributes
-        let allAttributeNames = Set(attributeMap.keys)
-        let requiredAttributeNames = allAttributeNames.filter { attributeMap[$0]!.isRequired }
-        let optionalAttributeNames = allAttributeNames.subtracting(requiredAttributeNames)
-        let requiredTransformations: [TransformationFromJSON] = requiredAttributeNames.map {
-            TransformationFromJSON.create(forAttributeNamed: $0, inAttributeMap: attributeMap)
-        }
-        let optionalTransformations: [TransformationFromJSON] = optionalAttributeNames.map {
-            TransformationFromJSON.create(forAttributeNamed: $0, inAttributeMap: attributeMap)
-        }
-        return SwiftFailableInitializer(requiredTransformations: requiredTransformations,
-                                        optionalTransformations: optionalTransformations)
-    }
-}
-
 // MARK: - JSON attribute --> TransformationFromJSON
-fileprivate extension TransformationFromJSON {
+extension TransformationFromJSON {
     static func create(forAttributeNamed attributeName: String, inAttributeMap attributeMap: JSONAttributeMap) -> TransformationFromJSON {
         let propertyName = attributeName.toSwiftPropertyName()
         var attributeName = attributeName
